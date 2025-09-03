397ba19e41260b9f1e3b38d9427d4278da158f12
Revendus05\Revendus <thiagocandido.bending@gmail.com>
2025-09-03 10:15:34 -0300

    Adding SQLite

--- app/src/main/java/com/example/shoppinglistapp_android/MainActivity.java
+++ app/src/main/java/com/example/shoppinglistapp_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shoppinglistapp_android;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,8 @@ public class MainActivity extends Activity {
     private ListView minhaLista;
     private Button meuBotao;
 
+    private SQLiteDatabase bancoDeDados;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,28 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 
+                try{
+                    bancoDeDados = openOrCreateDatabase("ShoppingList", MODE_PRIVATE, null);
+                    bancoDeDados.execSQL("Create Table IF NOT EXISTS meusItens(id INTEGER PRIMARY KEY AUTOINCREMENT, item VARCHAR)");
+
+                    String newItem = meuTexto.getText().toString();
+                    bancoDeDados.execSQL("INSERT INTO meusItens(tarefa) VALUES('" + newItem + "')");
+
+
+                    Cursor cursor = bancoDeDados.rawQuery("SELECT * FROM meusItens", null);
+
+                    int indiceColunaId = cursor.getColumnIndex("id");
+                    int indiceColunaItem = cursor.getColumnIndex("id");
+
+                    cursor.moveToFirst();
+                    while(cursor != null){
+                        Log.i("Logx", "ID: " + cursor.getString(indiceColunaId) + "Item: " + cursor.getString(indiceColunaItem));
+
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
